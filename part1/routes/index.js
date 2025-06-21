138b24371be30fa5b8e721ded7ca8d26e20b984b
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-21 01:51:28 +0000

    [automatic commit] 2025-06-21 01:51:27 UTC 129.127.202.85

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -5,5 +5,19 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/api/dogs', async (req, res) =>  {
+  try {
+    const query =`
+  SELECT d.name AS dog_name, size, u.username AS owner_username
+  FROM Dogs d
+  JOIN Users u ON d.owner_id = u.user_id`;
 
+  const [dogs] = await db.query(query);
+
+  res.json(dogs);
+  } catch (error) {
+    console.error('Error fetching dogs:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 module.exports = router;
