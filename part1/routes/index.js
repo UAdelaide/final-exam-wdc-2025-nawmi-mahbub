c5614d90f9163184676c2a86820c03423edae754
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-21 02:02:53 +0000

    [automatic commit] 2025-06-21 02:02:53 UTC 129.127.202.85

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -21,4 +21,51 @@ router.get('/api/dogs', async (req, res) =>  {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
+router.get('/api/walkrequests/open', async (req, res) => {
+  try {
+  const query =`
+  SELECT
+    w.request_id, d.name AS dog_name, w.requested_time, w.duration_minutes, w.location, u.username AS owner_username
+  FROM WalkRequests w
+  JOIN Dogs d ON d.dog_id = w.dog_id
+  JOIN Users u ON d.owner_id = u.user_id
+  WHERE w.status = 'open'`;
+
+  const [requests] = await db.query(query);
+
+  res.json(requests);
+  } catch (error) {
+    console.error('Error fetching walkrequests:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+router.get('/api/walkers/summary', async (req, res) => {
+  try {
+    const query = `
+      SELECT
+        u.username AS walker_username,
+        COUNT(r.rating_id) AS total_ratings,
+        ROUND(AVG(r.rating), 1) AS average_rating,
+        COUNT(CASE WHEN w.status = 'completed' THEN 1 END) AS completed_walks
+      FROM
+        Users u
+      LEFT JOIN WalkApplications a ON u.user_id = a.walker_id
+      LEFT JOIN WalkRequests w ON a.request_id = w.request_id AND w.status = 'completed'
+      LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
+      WHERE
+        u.role = 'walker'
+      GROUP BY
+        u.username
+      ORDER BY
+        u.username
+    `;
+
+    const [summary] = await db.query(query);
+    res.json(summary);
+  } catch (error) {
+    console.error('Error fetching summary:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 module.exports = router;
